36f4241a8c7c2dab52b5e3462fade3549abe4e01
Fosssil <dahiyabadshah.fossil@gmail.com>
2024-09-25 15:33:22 +0530

    25-Sep-2024 03:33 PM

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 vim.cmd("set showmatch")
+vim.cmd("set relativenumber")
 vim.cmd("set ignorecase")
 vim.cmd("set hlsearch")
 vim.cmd("set incsearch")
@@ -22,21 +23,19 @@ vim.g.mapleader = " "
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 local opts = {}
 
 require("lazy").setup("plugins")
-
-
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,30 +1,43 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls", "ansiblels", "bashls", "pylsp", "cssls", "yamlls" },
-        })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require('lspconfig')
-            lspconfig.lua_ls.setup({})
-            lspconfig.ansiblels.setup({})
-            lspconfig.bashls.setup({})
-            lspconfig.pylsp.setup({})
-            lspconfig.cssls.setup({})
-            lspconfig.yamlls.setup({})
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-        end
-    }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ansiblels",
+					"bashls",
+					"pylsp",
+					"cssls",
+					"yamlls",
+					"clangd",
+					"html",
+					"harper_ls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.ansiblels.setup({})
+			lspconfig.bashls.setup({})
+			lspconfig.pylsp.setup({})
+			lspconfig.cssls.setup({})
+			lspconfig.yamlls.setup({})
+			lspconfig.clangd.setup({})
+			lspconfig.html.setup({})
+			lspconfig.harper_ls.setup({})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
