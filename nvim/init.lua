d0984c9b0c70e3ee3f6273cc5bdcc62e09a18a10
Fosssil <dahiyabadshah.fossil@gmail.com>
2025-07-31 02:40:30 +0530

    31-Jul-2025 02:40 AM

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,49 +1,24 @@
-vim.cmd("set showmatch")
-vim.cmd("set relativenumber")
-vim.cmd("set ignorecase")
-vim.cmd("set hlsearch")
-vim.cmd("set incsearch")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set expandtab")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set autoindent")
-vim.cmd("set number")
-vim.cmd("set wildmode=longest,list")
-vim.cmd("set mouse=")
-vim.cmd("set clipboard=unnamedplus")
-vim.cmd("set cursorline")
-vim.cmd("set ttyfast")
-vim.cmd("syntax on")
-vim.cmd("filetype plugin on")
-vim.cmd("filetype plugin indent on")
+-- Faster startup
+vim.loader.enable()
 
-vim.g.mapleader = " "
+-- Reduce lag
+vim.o.lazyredraw = true
+vim.o.synmaxcol = 240
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
-end
-vim.opt.rtp:prepend(lazypath)
-local opts = {}
+-- Optimize GC
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 400
 
+-- Garbage collection
+vim.schedule(function()
+    collectgarbage("setpause", 100)
+    collectgarbage("setstepmul", 200)
+end)
+-- Bootstrap lazy.nvim and load plugins
+require("core.keymaps")
+require("core.lazy")
 require("lazy").setup("plugins")
 
-vim.cmd([[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]])
-vim.cmd([[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]])
-vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]])
-vim.cmd([[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]])
-vim.cmd([[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]])
-vim.cmd([[let g:terraform_fmt_on_save=1]])
-vim.cmd([[let g:terraform_align=1]])
+-- Load core settings
+require("core.options")
+require("core.disable")
---
+++ nvim/lua/plugins/lint.lua
@@ -0,0 +1,69 @@
+-- ~/.config/nvim/lua/plugins/lint.lua
+return {
+	{
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPost", "BufNewFile" },
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			local lint = require("lint")
+
+			-- Setup linters by filetype
+			lint.linters_by_ft = {
+				lua = { "luacheck" },
+				bash = { "shellcheck" },
+				sh = { "shellcheck" },
+				html = { "htmlhint" },
+				css = { "stylelint" },
+				json = { "jsonlint" },
+			}
+
+			-- Global diagnostic config
+			vim.diagnostic.config({
+				virtual_text = false,
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+				float = {
+					border = "rounded",
+				},
+			})
+
+			-- Faster CursorHold
+			vim.o.updatetime = 250
+
+			-- Float window on CursorHold
+			vim.api.nvim_create_autocmd("CursorHold", {
+				callback = function()
+					vim.diagnostic.open_float(nil, { focus = false })
+				end,
+			})
+
+			-- Clear diagnostics before linting to avoid duplicates
+			local function clear_and_lint()
+				vim.diagnostic.reset(nil, 0) -- Clear diagnostics for current buffer
+				lint.try_lint()
+			end
+
+			-- Trigger linting on save
+			-- vim.api.nvim_create_autocmd("BufWritePost", {
+			-- callback = function()
+			-- if not vim.g.disable_linting then
+			-- lint.try_lint()
+			-- end
+			-- end,
+			-- })
+			--
+			-- Trigger linting on save
+			vim.api.nvim_create_autocmd("BufWritePost", {
+				group = vim.api.nvim_create_augroup("Linting", { clear = true }),
+				callback = clear_and_lint,
+			})
+
+			-- Manual lint command
+			vim.api.nvim_create_user_command("Lint", function()
+				lint.try_lint()
+			end, { desc = "Lint current buffer" })
+		end,
+	},
+}
